88717cb4a85d58396fe166f1ddb544b68af7585f
giulibrumatti <giulibrumatti97@gmail.coim>
2024-02-07 14:03:58 -0300

    Iniciar sesión

--- my-app-gr/src/app/page.tsx
+++ my-app-gr/src/app/page.tsx
@@ -1,13 +1,19 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { AuthButton } from './components/auth.botton'
 
-export default function Home () {
+export default async function Home () {
   const supabase = createServerComponentClient({ cookies })
-  const { data: post } = await supabase.from('post').select()
+  const { data: posts } = await supabase.from('post').select('*')
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hola twitter
+      <AuthButton />
+      <pre>
+        {
+          JSON.stringify(posts, null, 2)
+        }
+      </pre>
     </main>
   )
 }
